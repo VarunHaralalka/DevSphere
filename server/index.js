e221505c3d9a26ce0602255dafb416317fee1e56
Varun Haralalka <varunharalalka@gmail.com>
2025-07-31 22:40:49 +0530

    Deployment fixes

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://devsphere-je3i.onrender.com",
     credentials: true,
     optionsSuccessStatus: 200,
   })
